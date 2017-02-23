a522ad4f8604dc6ea975788e19a5be794cedf2e8
Gordon Hall <gordon@gordonwritescode.com>
2017-02-23 14:48:49 -0500

    mass code exodus and new scaffolding

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 /**
  * @module kad
- * @license GPL-3.0
+ * @license AGPL-3.0
  * @author Gordon Hall https://github.com/bookchin
  */
 
 'use strict';
 
+/**
+ * Returns a new {@link KademliaNode}
+ * @function
+ */
+module.exports = function(options) {
+  return new module.exports.KademliaNode(options);
+};
+
+/** {@link AbstractNode} */
+module.exports.AbstractNode = require('./lib/node-abstract');
+
 /** {@link Bucket} */
 module.exports.Bucket = require('./lib/bucket');
 
-/** {@link module:kad/utils} */
-module.exports.utils = require('./lib/utils');
-
-/** {@link module:kad/constants} */
+/** {@link module:constants} */
 module.exports.constants = require('./lib/constants');
+
+/** {@link KademliaNode} */
+module.exports.KademliaNode = require('./lib/node-kademlia');
+
+/** {@link KademliaRules} */
+module.exports.KademliaRules = require('./lib/rules-kademlia');
+
+/** {@link MessageProcessor} */
+module.exports.MessageProcessor = require('./lib/message-processor');
+
+/** {@link RoutingTable} */
+module.exports.RoutingTable = require('./lib/routing-table');
+
+/** {@link module:utils} */
+module.exports.utils = require('./lib/utils');
