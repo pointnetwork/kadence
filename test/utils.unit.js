7337f21e07f73da8ef366dd4ff4d394ab8bc60b4
Emery Rose Hall <emery@deadcanaries.org>
2017-03-07 19:43:39 -0500

    add test files for scaffolding, add bucket tests, stub out utils tests, fix some bucket bugs

---
+++ test/utils.unit.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { expect } = require('chai');
+const utils = require('../lib/utils');
+
+
+describe('@module utils', function() {
+
+  describe('@function getRandomKeyString');
+
+  describe('@function getRandomKeyBuffer');
+
+  describe('@function keyStringIsValid');
+
+  describe('@function keyBufferIsValid');
+
+  describe('@function getDistance');
+
+  describe('@function compareKeyBuffers');
+
+  describe('@function getBucketIndex');
+
+  describe('@function getPowerOfTwoBufferForIndex');
+
+  describe('@function getRandomBufferInBucketRange');
+
+  describe('@function validateStorageAdapter');
+
+  describe('@function validateLogger');
+
+  describe('@function validateTransport');
+
+});
