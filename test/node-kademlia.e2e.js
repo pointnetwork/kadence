0289f22a6b379b88a8100f524261c5abcb02419f
Gordon Hall <gordon@gordonwritescode.com>
2017-03-16 16:24:11 -0400

    fix value encoding issues, updateContact bug, add end to end tests

---
+++ test/node-kademlia.e2e.js
@@ -0,0 +1,125 @@
+'use strict';
+
+const { expect } = require('chai');
+const kad = require('..');
+const network = require('./fixtures/node-generator');
+const async = require('async');
+
+
+const TOTAL_NODES = 32;
+
+describe('Kad-E2E', function() {
+
+  let nodes, seed, pairs;
+
+  before(function(done) {
+    nodes = network(TOTAL_NODES, kad.UDPTransport);
+    async.each(nodes, (node, done) => {
+      node.listen(node.contact.port, node.contact.hostname, done);
+    }, function() {
+      seed = nodes.shift();
+      nodes.forEach((node) => {
+        seed.router.addContactByNodeId(
+          node.identity.toString('hex'),
+          node.contact
+        );
+      });
+      pairs = nodes.map(() => {
+        return [
+          kad.utils.getRandomKeyString(),
+          kad.utils.getRandomKeyString()
+        ];
+      });
+      done();
+    });
+  });
+
+  after(function() {
+    nodes.forEach((node) => node._transport.socket.close());
+  });
+
+  describe('@method join', function() {
+
+    it('all nodes should succeed in joining the network', function(done) {
+      async.eachLimit(nodes, 6, function(node, next) {
+        node.join([
+          seed.identity.toString('hex'),
+          seed.contact
+        ], next);
+      }, function(err) {
+        expect(err).to.equal(null);
+        nodes.forEach((node) => {
+          expect(node.router.size > TOTAL_NODES / 2).to.equal(true);
+        });
+        done();
+      });
+    });
+
+  });
+
+  describe('@method iterativeFindNode', function() {
+
+    it('all nodes should be able to find K contacts', function(done) {
+      async.eachLimit(nodes, 6, function(node, next) {
+        node.iterativeFindNode(
+          node.identity.toString('hex'),
+          function(err, result) {
+            expect(err).to.equal(null);
+            expect(result).to.have.lengthOf(kad.constants.K);
+            next();
+          }
+        );
+      }, done);
+    });
+
+  });
+
+  describe('@method iterativeStore', function() {
+
+    it('all nodes should be able to store key-values', function(done) {
+      async.eachOfLimit(nodes, 6, function(node, index, next) {
+        let [key, value] = pairs[index];
+        node.iterativeStore(key, value, function(err, totalStored) {
+          expect(totalStored).to.equal(20);
+          next();
+        });
+      }, done);
+    });
+
+  });
+
+  describe('@method iterativeFindValue', function() {
+
+    it('all nodes should be able to retrieve key-values', function(done) {
+      async.eachOfLimit(nodes, 6, function(node, index, next) {
+        let [key, value] = pairs[index];
+        node.iterativeFindValue(key, function(err, result) {
+          expect(value).to.equal(result.value);
+          next();
+        });
+      }, done);
+    });
+
+  });
+
+  describe('@method expire / @method replicate', function() {
+
+    before(function(done) {
+      async.eachLimit(nodes, 6, function(node, next) {
+        node.replicate(() => node.expire(() => next()));
+      }, done);
+    });
+
+    it('all nodes should be able to retrieve after expire', function(done) {
+      async.eachOfLimit(nodes, 6, function(node, index, next) {
+        let [key, value] = pairs[index];
+        node.iterativeFindValue(key, function(err, result) {
+          expect(value).to.equal(result.value);
+          next();
+        });
+      }, done);
+    });
+
+  });
+
+});
