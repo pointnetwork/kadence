688c494183eae5f239c0201e5f547c31465c9d8e
Emery Rose Hall <emery@deadcanaries.org>
2018-03-04 20:27:41 -0500

    cover permissions plugin, fix replication when in use, remove join fail on 1 peer

--- test/plugin-permission.unit.js
+++ test/plugin-permission.unit.js
@@ -4,9 +4,10 @@ const { expect } = require('chai');
 const permission = require('../lib/plugin-permission');
 const crypto = require('crypto');
 const constants = require('../lib/constants');
+const utils = require('../lib/utils');
 
-constants.SOLUTION_DIFFICULTY = 8;
 
+constants.SOLUTION_DIFFICULTY = 4;
 
 describe('@module kadence/permission', function() {
 
@@ -20,9 +21,23 @@ describe('@module kadence/permission', function() {
       solver.once('data', data => {
         const { solution } = data;
         expect(solution).to.be.instanceOf(permission.PermissionSolution);
-        expect(solution.result[0]).to.equal(0);
+        expect(utils.satisfiesDifficulty(solution.result,
+          constants.SOLUTION_DIFFICULTY)).to.equal(true);
         done();
       });
+    });
+
+  });
+
+  describe('@class PermissionSolution', function() {
+
+    it('should pack into a hex tuple', function() {
+      const sol = new permission.PermissionSolution(
+        Buffer.alloc(260).fill(0)
+      );
+      const packed = sol.pack();
+      expect(packed[0]).to.have.lengthOf(40);
+      expect(packed[1]).to.have.lengthOf(520);
     });
 
   });
